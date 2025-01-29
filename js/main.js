aac152350954925568c9d4739dd82060edac9672
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-29 18:08:46 +0000

    Navbar_js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+    var nave = $ ('nav');
+var navHeight = nav.outerHeight();
+
+$('.navbar-toggler').on('click', function(){
+    if(! $('#mainNav').hasClass('navbar-reduce')){
+        $('#mainNav').addClass('navbar-reduce');
+    }
+})
+// PreLoader
+})

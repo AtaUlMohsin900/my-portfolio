bb1736633f32147e5f1831f2052ebad3c0deb0c7
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-31 11:10:49 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
 (function ($) {
-    "use strict";
-    var nave = $ ('nav');
-var navHeight = nav.outerHeight();
-
-$('.navbar-toggler').on('click', function(){
-    if(! $('#mainNav').hasClass('navbar-reduce')){
-        $('#mainNav').addClass('navbar-reduce');
+	"use strict";
+	var nav = $('nav');
+  var navHeight = nav.outerHeight();
+  
+  $('.navbar-toggler').on('click', function() {
+    if( ! $('#mainNav').hasClass('navbar-reduce')) {
+      $('#mainNav').addClass('navbar-reduce');
     }
-})
-// PreLoader
-$(window).on('load', function () {
+  })
+
+// Preloader
+  $(window).on('load', function () {
     if ($('#preloader').length) {
       $('#preloader').delay(100).fadeOut('slow', function () {
         $(this).remove();
       });
     }
   });
-//    Back to top Button
-$(window).scroll(function(){
-    if ($(this).scrollTop() > 100){
-        $('.back-to-top').fadIn('slow');
-    }else {
-        $('.back-to-top').fadeOut('slow');
-    }
-});
-$('html, body').animate({scrollTop : 0},1500,'easeInOutExpo');
-return false;
-});
 
+ // Back to top button
+ $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
 // Star Scroll Top
 $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function(){
     if (location.pathname.replace(/^\//,'')== this.pathname.replace(/^\//,'')&& location.hostname == this.hostname){
@@ -104,6 +106,7 @@ if ($('.text-slider').length == 1) {
 			backSpeed: 30
 		});
 	}
+
     /*--/ Testimonials owl /--*/
 	$('#testimonial-mf').owlCarousel({
 		margin: 20,

d26fac08be0304a1e26ad1c9d64551345471ed02
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-31 04:48:03 +0000

    Navbar Menu Reduce

--- js/main.js
+++ js/main.js
@@ -72,3 +72,23 @@ $('.js-scroll').on("click", function () {
 		target: '#mainNav',
 		offset: navHeight
 	});
+// End Scroll nav
+
+/*--/ Navbar Menu Reduce /--*/
+$(window).trigger('scroll');
+$(window).on('scroll', function () {
+    var pixels = 50; 
+    var top = 1200;
+    if ($(window).scrollTop() > pixels) {
+        $('.navbar-expand-md').addClass('navbar-reduce');
+        $('.navbar-expand-md').removeClass('navbar-trans');
+    } else {
+        $('.navbar-expand-md').addClass('navbar-trans');
+        $('.navbar-expand-md').removeClass('navbar-reduce');
+    }
+    if ($(window).scrollTop() > top) {
+        $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+    } else {
+        $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+    }
+});

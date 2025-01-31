e9f5385ce07b2f2619f1aa8e3f4de64551cbe2d3
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-31 04:48:51 +0000

    Star Typed

--- js/main.js
+++ js/main.js
@@ -92,3 +92,15 @@ $(window).on('scroll', function () {
         $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
     }
 });
+
+/*--/ Star Typed /--*/
+if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+		var typed = new Typed('.text-slider', {
+			strings: typed_strings.split(','),
+			typeSpeed: 80,
+			loop: true,
+			backDelay: 1100,
+			backSpeed: 30
+		});
+	}

e1a54d6a97b7173dad034ce4011814e19d5bd6eb
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-02-04 18:41:13 +0000

    update

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -11,3 +11,26 @@ jQuery(document).ready(function($) {
   
         var i = $(this); // current input
         var rule = i.attr('data-rule');
+        if (rule !== undefined) {
+            var ierror = false; // error flag for current input
+            var pos = rule.indexOf(':', 0);
+            if (pos >= 0) {
+              var exp = rule.substr(pos + 1, rule.length);
+              rule = rule.substr(0, pos);
+            } else {
+              rule = rule.substr(pos + 1, rule.length);
+            }
+    
+            switch (rule) {
+              case 'required':
+                if (i.val() === '') {
+                  ferror = ierror = true;
+                }
+                break;
+    
+              case 'minlen':
+                if (i.val().length < parseInt(exp)) {
+                  ferror = ierror = true;
+                }
+                break;
+    

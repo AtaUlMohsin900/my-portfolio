bb2d91614ece0ca6e034d450cd4e848f420ffdcc
ataulmohsin9000 <ataulmohisn9000@gmail.com>
2025-08-18 00:13:47 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Layout from "@/components/layout/Layout"
 import Brands1 from "@/components/sections/Brands1"
 import Home2 from "@/components/sections/Home2"
-import Resume1 from "@/components/sections/Resume1"
 import Testimonials1 from "@/components/sections/Testimonials1"
 import Coporation2 from "@/components/sections/Coporation2"
 import Static2 from "@/components/sections/Static2"
@@ -9,14 +8,16 @@ import Service2 from "@/components/sections/Service2"
 import Projects2 from "@/components/sections/Projects2"
 import Skills2 from "@/components/sections/Skills2"
 import Blog2 from "@/components/sections/Blog2"
-import Contact2 from "@/components/sections/Contact2"
 import Experience2 from "@/components/sections/Experience2"
 import Education2 from "@/components/sections/Education2"
+import Contact2 from "@/components/sections/Contact2"
+
+
 export default function Home() {
 
 	return (
 		<>
-			<Layout headerStyle={1} footerStyle={1}>
+			<Layout headerStyle={2} footerStyle={2}>
 				<Home2 />
 				<Static2 />
 				<Coporation2 />
@@ -24,10 +25,9 @@ export default function Home() {
 				<Experience2 />
 				<Education2 />
 				<Projects2 />
-				<Resume1 />
 				<Skills2 />
-				<Brands1 />
-				<Testimonials1 />
+				{/* <Brands1 />
+				<Testimonials1 /> */}
 				<Blog2 />
 				<Contact2 />
 			</Layout>

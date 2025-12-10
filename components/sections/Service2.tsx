5966df66afd2443f107a41cf817bd3d412f1740a
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-12-10 19:16:09 +0000

    update

--- components/sections/Service2.tsx
+++ components/sections/Service2.tsx
@@ -32,7 +32,7 @@ export default function Service2() {
 												<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
 													<path d="M21 3C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3H21ZM20 11H4V19H20V11ZM20 5H4V9H20V5ZM11 6V8H9V6H11ZM7 6V8H5V6H7Z" fill="#1F1F24" />
 												</svg>
-												<h6 className="my-3 fw-medium">Web &amp; App Development</h6>
+												<h6 className="my-3 fw-medium">Software &amp; Development</h6>
 												<p className="fs-7 text-300 fw-regular">Crafting visually appealing and user-friendly interfaces using <span className="text-secondary-2">HTML</span>, <span className="text-secondary-2">CSS</span>, <span className="text-secondary-2">JavaScript</span>, and modern frameworks like React and Angular.</p>
 											</div>
 										</div>
